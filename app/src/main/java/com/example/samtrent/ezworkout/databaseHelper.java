add5a21a67a48c7a10f382bf8a041472b740ca67
Arturo Aguila <churro777@gmail.com>
2016-11-15 11:40:31 -0700

    Wrote the Create statments for all the tables

--- app/src/main/java/com/example/samtrent/ezworkout/databaseHelper.java
+++ app/src/main/java/com/example/samtrent/ezworkout/databaseHelper.java
@@ -26,30 +26,46 @@ public class databaseHelper extends SQLiteOpenHelper {
     private static final String KEY_W_PROCEDURE = "Procedure";
     private static final String KEY_W_IS_FAVORITE = "isFavorite";
     private static final String KEY_W_MUSCLE_GROUP = "Muscle Group";
+    // Workouts table create
+    private static final String CREATE_WORKOUTS_TABLE =
+            "CREATE TABLE " + TABLE_WORKOUTS +
+                    "(" + KEY_W_ID + " INTEGER PRIMARY KEY," +
+                    KEY_W_NAME + " TEXT, " +
+                    KEY_W_PROCEDURE + " TEXT, " +
+                    KEY_W_MUSCLE_GROUP + " INTEGER, " +
+                    KEY_W_IS_FAVORITE + " INTEGER)";
 
-    // Workouts_list table and column names
+    // Workouts_lists table and column names
     private static final String TABLE_WORKOUT_LIST = "Workout_Lists";
     private static final String KEY_WL_ID = "ID";
     private static final String KEY_WL_MUSCLE_GROUP = "Muscle Group";
     private static final String KEY_WL_PLACE = "Place";
+    // Workouts_list table create
+    private static final String CREATE_WORKOUTS_LIST_TABLE =
+            "CREATE TABLE " + TABLE_WORKOUT_LIST +
+                    "(" + KEY_WL_ID + " INTEGER PRIMARY KEY," +
+                    KEY_WL_MUSCLE_GROUP + " INTEGER, " +
+                    KEY_WL_PLACE + " INTEGER)";
 
     // Places table and column names
     private static final String TABLE_PLACE = "Places";
     private static final String KEY_P_ID = "ID";
     private static final String KEY_P_PLACE = "Place";
+    // Places table create
+    private static final String CREATE_PLACE_TABLE =
+            "CREATE TABLE " + TABLE_PLACE +
+                    "(" + KEY_P_ID + " INTEGER PRIMARY KEY," +
+                    KEY_P_PLACE + " TEXT)";
 
     // Favorite Workouts table and column names
     private static final String TABLE_FAVORITE_WORKOUTS = "Favorite_Workouts";
     private static final String KEY_FW_ID = "ID";
     private static final String KEY_FW_WORKOUT = "Workout";
-
-    private static final String CREATE_WORKOUTS_TABLE = "CREATE TABLE " + TABLE_WORKOUTS +
-            "(" + KEY_W_ID + " INTEGER PRIMARY KEY," +
-            KEY_W_NAME + " TEXT, " +
-            KEY_W_PROCEDURE + " TEXT, " +
-            KEY_W_MUSCLE_GROUP + " TEXT, " +
-            KEY_W_IS_FAVORITE + " INTEGER)";
-
+    // Favorite Workouts table create
+    private static final String CREATE_FAV_WORKOUTS_TABLE =
+            "CREATE TABLE " + TABLE_FAVORITE_WORKOUTS +
+                    "(" + KEY_FW_ID + " INTEGER PRIMARY KEY," +
+                    KEY_FW_WORKOUT + " INTEGER)";
 
 
     public databaseHelper(Context context) {
