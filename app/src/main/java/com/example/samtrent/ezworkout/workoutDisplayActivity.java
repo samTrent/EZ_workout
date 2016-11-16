bafa7379cf2428dee3c92a0259e97246b1dbbc26
Sam Trent <samtrent@Sams-MacBook-Air.local>
2016-11-16 09:24:15 -0700

    added logs to check for errors. Also reformatted displayWorkout

--- app/src/main/java/com/example/samtrent/ezworkout/workoutDisplayActivity.java
+++ app/src/main/java/com/example/samtrent/ezworkout/workoutDisplayActivity.java
@@ -1,81 +1,54 @@
 package com.example.samtrent.ezworkout;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Movie;
-import android.graphics.Rect;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-import java.io.InputStream;
+import java.io.IOException;
+
+import pl.droidsonroids.gif.GifDrawable;
 
 public class workoutDisplayActivity extends AppCompatActivity {
 
+    // creating our "gif" object
+    GifDrawable gifAnim;
+
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_display);
+
+
+        // check to see if the gif file exsists...
+
+
+
+            //if successful, create the gif object with the gif file as a param
+
+
+        try {
+
+            gifAnim = new GifDrawable(getResources(), R.drawable.hammercurl);
+            gifAnim.setTint(10);
+
+        } catch (IOException e)
+        {
+            Log.e("make gif", "onCreate: could not generate gif", e);
+            
+            e.printStackTrace();
+        }
+
     }
 
-}
-// ***** WORKING ON GIF PLAYER FUNCTION *****
 
-//    public class AA extends Activity
-//    {
-//
-//        @Override
-//        public void onCreate(Bundle savedInstanceState)
-//        {
-//
-//            super.onCreate(savedInstanceStated);
-//
-//
-//            setContentView(new MYGIFView(this));
-//        }
-//
-//    }
-//
-//
-//    private class MYGIFView extends View
-//    {
-//
-//        Movie movie, movie1;
-//        InputStream is = null, is1 = null;
-//        long moviestart;
-//
-//        public GIFView(Context context)
-//        {
-//            super(context);
-//
-//
-//        }
-//
-//
-//    }
-//
-//    is=context.getResources().openRawResource(R.drawable.earth);
-//    movie=Movie.decodeStream(is);
-//
-//
-//
-//    @Override
-//    protected void onDraw(Canvas canvas)
-//    {
-//
-//        canvas.drawColor(Color.WHITE);
-//        super.onDraw(canvas);
-//        long now=android.os.SystemClock.uptimeMillis();
-//        System.out.println("now="+now);
-//        if (moviestart == 0)
-//        { // first time
-//            moviestart = now;
-//
-//        }
-//}
+
+
+
+
+}
 
 // test to make sure that we are getting a gif file.
 class GifFileTester
